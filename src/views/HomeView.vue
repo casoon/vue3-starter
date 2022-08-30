e212dc2f157000bca5048399361727d89457d75f
casoon <joern.seidel@casoon.de>
2022-08-30 17:13:58 +0200

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <!-- ========== MAIN CONTENT ========== -->
+  <div class="max-w-[90rem] mx-auto">
+    <!-- Sidebar Toggle -->
+    <div class="sticky top-0 inset-x-0 z-20 bg-white border-y px-4 sm:px-6 md:px-8 lg:hidden dark:bg-slate-900 dark:border-gray-700">
+      <div class="max-w-3xl mx-auto py-2">
+        <!-- Navigation Toggle -->
+        <button type="button" class="flex justify-between gap-x-2 items-center w-full text-gray-500 hover:text-gray-600" data-hs-sidebar="#docs-sidebar" aria-controls="docs-sidebar" aria-label="Toggle navigation">
+          <span class="text-sm">Toggle Navigation</span>
+          <svg class="w-5 h-5" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+          </svg>
+        </button>
+        <!-- End Navigation Toggle -->
+      </div>
+    </div>
+    <!-- End Sidebar Toggle -->
+
+    <!-- Sidebar -->
+    <div id="docs-sidebar" class="hs-sidebar hs-sidebar-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 left-0 bottom-0 z-[60] w-80 bg-white border-r border-gray-200 py-10 px-8 overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:top-0 lg:right-auto lg:bottom-0 lg:left-[max(0px,calc(50%-45rem))] lg:z-10 dark:scrollbar-y dark:bg-slate-900">
+      <!-- Navigation Toggle -->
+      <button type="button" class="ml-auto flex justify-end lg:hidden text-gray-500 hover:text-gray-600" data-hs-sidebar="#docs-sidebar" aria-controls="docs-sidebar" aria-label="Toggle navigation">
+        <span class="sr-only">Toggle Navigation</span>
+        <svg class="w-5 h-5" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+          <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+        </svg>
+      </button>
+      <!-- End Navigation Toggle -->
+
+      <!-- Nav -->
+      <nav id="sidebar-nav" class="relative space-y-8">
+        <h5 class="mb-3 text-sm font-semibold text-slate-900 dark:text-slate-200">Navbar Links</h5>
+        <ul class="ml-0.5 space-y-2 border-l-2 border-slate-100 dark:border-slate-800" data-hs-scrollspy="#scrollspy">
+          <li><a class="block py-1 pl-4 -ml-px border-l-2 border-transparent text-sm text-slate-700 hover:border-slate-400 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300 hs-scrollspy-active:font-medium hs-scrollspy-active:text-blue-600 dark:hs-scrollspy-active:text-blue-400 active" href="#first">First</a></li>
+          <li><a class="block py-1 pl-4 -ml-px border-l-2 border-transparent text-sm text-slate-700 hover:border-slate-400 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300 hs-scrollspy-active:font-medium hs-scrollspy-active:text-blue-600 dark:hs-scrollspy-active:text-blue-400" href="#second">Second</a></li>
+          <li><a class="block py-1 pl-4 -ml-px border-l-2 border-transparent text-sm text-slate-700 hover:border-slate-400 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300 hs-scrollspy-active:font-medium hs-scrollspy-active:text-blue-600 dark:hs-scrollspy-active:text-blue-400" href="#third">Third</a></li>
+          <li><a class="block py-1 pl-4 -ml-px border-l-2 border-transparent text-sm text-slate-700 hover:border-slate-400 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300 hs-scrollspy-active:font-medium hs-scrollspy-active:text-blue-600 dark:hs-scrollspy-active:text-blue-400" href="#fourth">Fourth</a></li>
+          <li><a class="block py-1 pl-4 -ml-px border-l-2 border-transparent text-sm text-slate-700 hover:border-slate-400 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300 hs-scrollspy-active:font-medium hs-scrollspy-active:text-blue-600 dark:hs-scrollspy-active:text-blue-400" href="#fifth">Fifth</a></li>
+          <li><a class="block py-1 pl-4 -ml-px border-l-2 border-transparent text-sm text-slate-700 hover:border-slate-400 hover:text-slate-900 dark:text-slate-400 dark:hover:text-slate-300 hs-scrollspy-active:font-medium hs-scrollspy-active:text-blue-600 dark:hs-scrollspy-active:text-blue-400" href="#sixth">Sixth</a></li>
+        </ul>
+      </nav>
+      <!-- Nav -->
+    </div>
+    <!-- End Sidebar -->
+
+    <!-- Content -->
+    <div class="w-full px-4 sm:px-6 md:px-8 lg:pl-[22rem]">
+      <div class="max-w-3xl mx-auto xl:max-w-none py-10 xl:ml-0 xl:mr-64 xl:pr-16">
+
+        <!-- Content -->
+        <div class="mt-16">
+          <div id="scrollspy" class="space-y-10 md:space-y-16">
+            <div id="first" class="scroll-mt-24 min-h-[25rem]">
+              <h2 class="text-lg font-semibold text-gray-800 dark:text-white">First section</h2>
+            </div>
+
+            <div id="second" class="scroll-mt-24 min-h-[25rem]">
+              <h2 class="text-lg font-semibold text-gray-800 dark:text-white">Second section</h2>
+            </div>
+
+            <div id="third" class="scroll-mt-24 min-h-[25rem]">
+              <h2 class="text-lg font-semibold text-gray-800 dark:text-white">Third section</h2>
+            </div>
+
+            <div id="fourth" class="scroll-mt-24 min-h-[25rem]">
+              <h2 class="text-lg font-semibold text-gray-800 dark:text-white">Fourth section</h2>
+            </div>
+
+            <div id="fifth" class="scroll-mt-24 min-h-[25rem]">
+              <h2 class="text-lg font-semibold text-gray-800 dark:text-white">Fifth section</h2>
+            </div>
+
+            <div id="sixth" class="scroll-mt-24 min-h-[25rem]">
+              <h2 class="text-lg font-semibold text-gray-800 dark:text-white">Sixth section</h2>
+            </div>
+          </div>
+        </div>
+        <!-- End Content -->
+      </div>
+    </div>
+    <!-- End Content -->
+  </div>
+  <!-- ========== END MAIN CONTENT ========== -->
+</template>
---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "node_modules/preline/dist/*.js"
+  ],
+  theme: {
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('preline/plugin')
+  ],
+}
